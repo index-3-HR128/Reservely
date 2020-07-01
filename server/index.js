6dd8976bdc59f036e635d9b4edd05b01ca4a8b04
Benny Lee <lee.hsinchih@gmail.com>
2020-06-30 19:47:39 -0700

    first commit proxy server.  Added everyone's componenet without any extra styling

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const app = express()
+const port = 3000
+const path = require('path');
+
+app.use(express.static(path.join(__dirname,'..','client','dist')))
+
+app.all('/carousel', createProxyMiddleware({ target: 'http://localhost:3003'}));
+app.all('/photogallery', createProxyMiddleware({ target: 'http://localhost:3004'}));
+app.all('/reviews', createProxyMiddleware({ target: 'http://localhost:3002'}));
+app.all('/calendar', createProxyMiddleware({ target: 'http://localhost:3001'}));
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
