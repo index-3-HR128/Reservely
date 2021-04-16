10e179efe305f92c236e12798967aeddb7209d04
Benny Lee <lee.hsinchih@gmail.com>
2021-04-16 09:09:07 -0700

    mod readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Reservely
+
+<iframe width="420" height="315"
+src="https://youtu.be/3cMMQCXY5IU">
+</iframe>
--- proxy-server/server/index.js
+++ proxy-server/server/index.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express()
-const port = 3000
+const port = 3000;
 const path = require('path');
 
 app.use(express.static(path.join(__dirname,'..','client','dist')))
 
-app.all('/', createProxyMiddleware({ target: 'http://54.215.84.53/'}));
-// app.all('/photogallery', createProxyMiddleware({ target: 'http://localhost:3004'}));
-// app.all('/reviews', createProxyMiddleware({ target: 'http://localhost:3002'}));
-// app.all('/calendar', createProxyMiddleware({ target: 'http://localhost:3001'}));
+app.all('/photogallery', createProxyMiddleware({ target: 'http://localhost:3004'}));
+app.all('/reviews', createProxyMiddleware({ target: 'http://localhost:3002'}));
+app.all('/calendar', createProxyMiddleware({ target: 'http://localhost:3001'}));
+app.all('/carousel', createProxyMiddleware({ target: 'http://localhost:3003'}));
+
+
+// app.all('/', createProxyMiddleware({ target: 'http://localhost:3003'}));
+// app.all('/', createProxyMiddleware({ target: 'http://localhost:3004'}));
+// app.all('/', createProxyMiddleware({ target: 'http://localhost:3002'}));
+// app.all('/', createProxyMiddleware({ target: 'http://localhost:3001'}));
+
 
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
